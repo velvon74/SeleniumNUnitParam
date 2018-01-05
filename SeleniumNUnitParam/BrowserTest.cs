1a5ff6a64775483b7f07d6b4365cc19d34238ff6
Alex Laptev <alex@laptev.me>
2018-01-05 14:35:03 +0200

    Fixed Google test

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -17,8 +17,8 @@ namespace SeleniumNUnitParam
         {
             Driver.Navigate().GoToUrl("http://www.google.com");
             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
-            System.Threading.Thread.Sleep(5000);
-            Driver.FindElement(By.Name("btnG")).Click();
+            System.Threading.Thread.Sleep(9000);
+            Driver.FindElement(By.Name("btnK")).Click();
             Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
                                             "The text selenium doest not exist");
 
@@ -31,7 +31,7 @@ namespace SeleniumNUnitParam
             Driver.FindElement(By.Name("UserName")).SendKeys("admin");
             Driver.FindElement(By.Name("Password")).SendKeys("admin");
             Driver.FindElement(By.Name("Login")).Submit();
-            System.Threading.Thread.Sleep(2000);
+            System.Threading.Thread.Sleep(9000);
             Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
                                             "The text selenium doest not exist");
 
